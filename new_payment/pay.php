e0fd5417177836bc43abc6ebf5aef26dbf7cc544
Akash Jaiswal <akashjas1996@gmail.com>
2019-09-27 16:32:47 +0530

    file renamed

--- new_payment/pay.php
+++ new_payment/pay.php
@@ -1,46 +1,29 @@
-<?php
-$data = $_POST;
-$mac_provided = $data['mac'];  /* Get the MAC from the POST data*/
-unset($data['mac']);  /* Remove the MAC key from the data. */
-$ver = explode('.', phpversion());
-$major = (int) $ver[0];
-$minor = (int) $ver[1];
-if($major >= 5 and $minor >= 4){
-     ksort($data, SORT_STRING | SORT_FLAG_CASE);
+<?php 
+$purpose = "Payment";
+$amount = $_POST["amount"];
+$name = $_POST["name"];
+$phone = $_POST["phone"];
+$email = $_POST["email"];
+include 'src/instamojo.php';
+$api = new Instamojo\Instamojo('29b34070bf5867b7d36bf2586c4f4855', '40d161c14f252cc781066e0c685f5f4d','https://www.instamojo.com/api/1.1/');
+try {
+    $response = $api->paymentRequestCreate(array(
+        "purpose" => $purpose,
+        "amount" => $amount,
+        "buyer_name" => $name,
+        "phone" => $phone,
+		"email" => $email,
+        "send_email" => true,
+        "send_sms" => true,
+        'allow_repeated_payments' => false,
+        "redirect_url" => "https://studentstutorial.com/instamojo/thankyou.php",
+        "webhook" => "https://studentstutorial.com/instamojo/webhook.php"
+        ));
+   $pay_ulr = $response['longurl'];
+    header("Location: $pay_ulr");
+    exit();
 }
-else{
-     uksort($data, 'strcasecmp');
-}
-/* You can get the 'salt' from Instamojo's developers page(make sure to log in first): https://www.instamojo.com/developers*/
-/* Pass the 'salt' without the <>.*/
-$mac_calculated = hash_hmac("sha1", implode("|", $data), "29ac91de833b49f9ab7a0bde653ac337");
-
-if($mac_provided == $mac_calculated){
-   
-    if($data['status'] == "Credit"){
-       /* Payment was successful, mark it as completed in your database  */
-                
-                $to = 'YOUR_EMAIL_ADDRESS';
-                $subject = 'Website Payment Request ' .$data['buyer_name'].'';
-                $message = "<h1>Payment Details</h1>";
-                $message .= "<hr>";
-                $message .= '<p><b>ID:</b> '.$data['payment_id'].'</p>';
-                $message .= '<p><b>Amount:</b> '.$data['amount'].'</p>';
-                $message .= "<hr>";
-                $message .= '<p><b>Name:</b> '.$data['buyer_name'].'</p>';
-                $message .= '<p><b>Email:</b> '.$data['email'].'</p>';
-                $message .= '<p><b>Phone:</b> '.$data['phone'].'</p>';
-                $message .= "<hr>";
-				$headers .= "MIME-Version: 1.0\r\n";
-                $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-                /* send email*/
-                mail($to, $subject, $message, $headers);
-		}
-    else{
-       /* Payment was unsuccessful, mark it as failed in your database*/
-    }
-}
-else{
-    echo "Invalid MAC passed";
-}
-?>
+catch (Exception $e) {
+    print('Error: ' . $e->getMessage());
+}     
+ ?>
