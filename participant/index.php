5a98816283ed422714ba6dec6c838401323a596f
Akash Jaiswal <akashjas1996@gmail.com>
2019-09-29 02:28:55 +0530

    sms 5000

--- participant/index.php
+++ participant/index.php
@@ -62,17 +62,22 @@ if(isset($_POST['otp_sent'])){
 
 $curl = curl_init();
 
+// curl_setopt_array($curl, array(
+//    CURLOPT_URL => "https://api.msg91.com/api/sendhttp.php?mobiles=".$mbl."&authkey=296366AFcEP9oki5d8fad56&route=4&sender=KRTRTH&message=".$otp." is your verification Code.",
+
+
+
+$curl = curl_init();
+
 curl_setopt_array($curl, array(
-   CURLOPT_URL => "https://api.msg91.com/api/sendhttp.php?mobiles=".$mbl."&authkey=296366AFcEP9oki5d8fad56&route=4&sender=KRTRTH&message=".$otp." is your verification Code.",
-
-
-
+  CURLOPT_URL => "https://control.msg91.com/api/sendotp.php?email=kritarthwebsite@gmail.com&template=hello&otp=".$otp."&otp_length=4&otp_expiry=10&sender=KRTRTH&message=".$otp." is the mobile Verification code for KRITARTH 5.0.&mobile=".$mbl."&authkey=296366AFcEP9oki5d8fad56",
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => "",
   CURLOPT_MAXREDIRS => 10,
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "",
   CURLOPT_SSL_VERIFYHOST => 0,
   CURLOPT_SSL_VERIFYPEER => 0,
 ));
@@ -85,8 +90,11 @@ curl_close($curl);
 if ($err) {
   echo "cURL Error #:" . $err;
 } else {
-   // echo $response;
+  //echo $response;
 }
+
+
+
 }
 
 
