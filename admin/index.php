e31741fe708607089777cdd6fefbb67d1bf50185
Akash Jaiswal <akashjas1996@gmail.com>
2019-09-30 18:57:57 +0530

    php error

--- admin/index.php
+++ admin/index.php
@@ -153,6 +153,7 @@ table.blueTable tfoot .links a{
 									echo '<td>'.$row_get_edit['event_name'].'</td>';
 									echo '<td>'.$row_get_edit['shedule'].'</td>';
 									echo '<td>'.$row_get_edit['venue'].'</td>';
+									?>
 									<?php var_dump($_POST); ?>
 									<td> <form method="POST" action="../checkReg" > 
 											<input type="hidden" name="eid" value="<?php echo $eid ?>"> 
