2693ed02d38a11abe66221ec41bd87f0c813ea48
Akash Jaiswal <akashjas1996@gmail.com>
2019-10-14 15:19:27 +0530

    ds

--- calltopay/index.php
+++ calltopay/index.php
@@ -208,9 +208,9 @@ table.blueTable tfoot .links a{
 							 $row_get_verify = mysqli_fetch_assoc($res_get_verify);
 							 $role = $row_get_email['role'];
 							 echo $role;
-							 if($role!=3){
-							 	redirect('https://kritarth.org/participant');
-							 }
+							 // if($role!=3){
+							 // 	redirect('https://kritarth.org/participant');
+							 // }
 							// echo $row_get_email['name']." !";
 						  ?></h2> </center>
 						  <?php $count=1; ?>
