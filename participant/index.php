999910a61d085d974661714261ebc779350301a0
Akash Jaiswal <akashjas1996@gmail.com>
2019-10-18 13:52:03 +0530

    error

--- participant/index.php
+++ participant/index.php
@@ -144,7 +144,7 @@ if(isset($_POST['roll_save'])){
 if(isset($_POST['participation_pressed'])){
 	$evnt =  $_POST['req_event'];
 	$kid =  $_POST['k_id'];
-	if($kid==$_SESSION['k_id']){
+	if($kid!=$_SESSION['k_id']){
 		redirect('https://kritarth.org/error.php');
 	}
 	$event_query = "SELECT * FROM pratispradha_chunao WHERE event_id='$evnt' AND kritarth_id = '$kid'";
