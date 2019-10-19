ca748cc1c2419966f9ba9eacb37c0c3f640f1c21
Akash Jaiswal <akashjas1996@gmail.com>
2019-10-19 09:17:48 +0530

    sms event details

--- sms.php
+++ sms.php
@@ -1,7 +1,7 @@
 <?php
 
 include 'inc/dbconnection.php';
-$query = "SELECT * FROM khata WHERE contact>0 and pass=0";
+$query = "SELECT * FROM khata WHERE contact>0 and pass=1 limit 2";
 $res_query = mysqli_query($link, $query);
 while($row_query = mysqli_fetch_assoc($res_query))
 {
@@ -19,10 +19,10 @@ $curl = curl_init();
 
 
 $curl = curl_init();
-echo $kid;
+$kid=2345;
 echo $mbl;
 curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://control.msg91.com/api/sendotp.php?template=hello&otp=".$kid."&otp_length=4&otp_expiry=10&sender=KRTRTH&message=".$kid." is your KID, please collect your Kritarth ID Card Today(18/10/2019) before 7 PM.&mobile=".$mbl."&authkey=296366AFcEP9oki5d8fad56",
+  CURLOPT_URL => "https://control.msg91.com/api/sendotp.php?template=hello&otp=".$kid."&otp_length=4&otp_expiry=10&sender=KRTRTH&message=".$kid." is your KID, please check your event venue and time here : http://kritarth.org/Events/ &mobile=".$mbl."&authkey=296366AFcEP9oki5d8fad56",
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => "",
   CURLOPT_MAXREDIRS => 10,
